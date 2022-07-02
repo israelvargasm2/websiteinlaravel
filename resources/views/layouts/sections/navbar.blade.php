a2094d5a584c8ad6e21ede3c18f49ed441ef1a4c
israelvargasm2 <israelvargasm2@gmail.com>
2022-07-02 01:21:33 -0500

    navbar button correction

--- resources/views/layouts/sections/navbar.blade.php
+++ resources/views/layouts/sections/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg bg-dark text-white">
         <div class="container-fluid text-white">
             <a class="navbar-brand text-white" href="{{ route('home') }}">@lang('messages.navbar-item-logo')</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            <button class="navbar-toggler" style="background: white;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
